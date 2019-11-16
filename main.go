43f3743572cf1117ebbe343f3c16a02fca286373
hanjinming <hanjinming@outlook.com>
2019-11-16 20:51:42 +0800

    增加server酱微信消息推送渠道

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/hanjm/errors"
+	"golang.org/x/sync/errgroup"
 	"gopkg.in/gomail.v2"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"sort"
 	"strconv"
@@ -27,7 +30,7 @@ type Fund struct {
 	Name string `json:"name"`
 	// 截止日期
 	JzRq string `json:"jzrq"`
-	// (昨日)单位净值 
+	// (昨日)单位净值
 	Dwjz float64 `json:"dwjz,string"`
 	// (当前)估算净值
 	Gsz float64 `json:"gsz,string"`
@@ -42,15 +45,17 @@ func (f Fund) String() string {
 }
 
 var (
-	httpClient = &http.Client{}
+	httpClient = &http.Client{
+		Timeout: time.Minute,
+	}
 	bodyPrefix = []byte("jsonpgz(")
 	bodySuffix = []byte(");")
 )
 
 // FetchFund
-func FetchFund(code string) (fund Fund, err error) {
+func FetchFund(ctx context.Context, code string) (fund Fund, err error) {
 	reqURL := "http://fundgz.1234567.com.cn/js/" + code + ".js"
-	req, err := http.NewRequest(http.MethodGet, reqURL, nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, reqURL, nil)
 	if err != nil {
 		err = errors.Errorf(err, "new request, url:%s", reqURL)
 		return
@@ -84,7 +89,7 @@ func FetchFund(code string) (fund Fund, err error) {
 }
 
 // FetchFunds
-func FetchFunds(codes []string) (funds []Fund, err error) {
+func FetchFunds(ctx context.Context, codes []string) (funds []Fund, err error) {
 	const concurrency = 3
 	limiter := make(chan struct{}, concurrency)
 	foundCh := make(chan Fund)
@@ -103,7 +108,7 @@ func FetchFunds(codes []string) (funds []Fund, err error) {
 				if code == "" {
 					return
 				}
-				fund, err := FetchFund(code)
+				fund, err := FetchFund(ctx, code)
 				if err != nil {
 					err = errors.Errorf(err, "code:%s", code)
 					select {
@@ -132,7 +137,7 @@ func FetchFunds(codes []string) (funds []Fund, err error) {
 }
 
 // GenerateEmailHTML
-func GenerateEmailHTML(funds []Fund, minRiseNum float64, maxFallNum float64) (emailHtml string, shouldSend bool) {
+func GenerateEmailHTML(ctx context.Context, funds []Fund, minRiseNum float64, maxFallNum float64) (emailHtml string, shouldSend bool) {
 	var elements []string
 	var content string
 	for _, fund := range funds {
@@ -194,7 +199,7 @@ var (
 	timeLocationCST = time.FixedZone("CST", 28800)
 )
 
-func SendEmail(content string, smtpHost string, emailName string, emailPassword string, emailTo string) (err error) {
+func SendEmail(ctx context.Context, content string, smtpHost string, emailName string, emailPassword string, emailTo string) (err error) {
 	if content == "" {
 		return
 	}
@@ -208,6 +213,83 @@ func SendEmail(content string, smtpHost string, emailName string, emailPassword 
 		err = errors.Errorf(err, "content:%s", content)
 		return err
 	}
+	return nil
+}
+
+// GenerateServerChanMessage
+func GenerateServerChanMessage(ctx context.Context, funds []Fund, minRiseNum float64, maxFallNum float64) (title string, body string, shouldSend bool) {
+	var elements []string
+	var content string
+	var fallCount int
+	for _, fund := range funds {
+		var status string
+		// 涨跌幅度超出设定值
+		if fund.Gszzl > 0 && fund.Gszzl >= minRiseNum {
+			status = "涨"
+		} else if fund.Gszzl < 0 && fund.Gszzl <= maxFallNum {
+			status = "跌"
+			fallCount++
+		} else {
+			status = "-"
+		}
+		element := `|` + status +
+			`|` + fund.Name +
+			`|` + strconv.FormatFloat(fund.Gszzl, 'f', -1, 64) +
+			`|` + strconv.FormatFloat(fund.Gsz, 'f', -1, 64) +
+			`|` + strconv.FormatFloat(fund.Dwjz, 'f', -1, 64) +
+			`|` + fund.Gztime +
+			`|`
+		elements = append(elements, element)
+	}
+	if len(elements) > 0 {
+		title = fmt.Sprintf("基金涨跌监控_%s_%d跌", time.Now().In(timeLocationCST).Format("2006-01-02"), fallCount)
+		content = strings.Join(elements, "\n")
+		body = `
+|状态|基金名称|估算涨幅|当前估算净值|昨日单位净值|估算时间|
+| -- | -- | -- | -- | -- | -- |
+` + content
+
+		return title, body, true
+	}
+	return "", "", false
+}
+
+func SendServerChan(ctx context.Context, title string, body string, serverChanKey string) (err error) {
+	if title == "" {
+		err = errors.Errorf(err, "empty title")
+		return
+	}
+	if serverChanKey == "" {
+		err = errors.Errorf(err, "empty serverChanKey")
+		return
+	}
+	if len(title) > 256 {
+		title = title[:256]
+	}
+	param := url.Values{}
+	param.Set("text", title)
+	param.Set("desp", body)
+	reqURL := fmt.Sprintf("https://sc.ftqq.com/%s.send", serverChanKey)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, reqURL, strings.NewReader(param.Encode()))
+	if err != nil {
+		err = errors.Errorf(err, "http newRequest")
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		err = errors.Errorf(err, "http get")
+		return
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+	if resp.StatusCode != http.StatusOK {
+		err = errors.Errorf(err, "server return %d", resp.StatusCode)
+		return
+	}
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	log.Printf("respBody:%s", respBody)
 	return nil
 }
 
@@ -233,23 +315,57 @@ func main() {
 		emailTo = emailName
 	}
 	log.Printf("emailTo:%+v", emailTo)
+	serverChanKey := os.Getenv("SCKEY")
+	// start do
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*2)
+	defer cancel()
 	// fetch funds data
-	fundResult, err := FetchFunds(strings.Split(fundCodes, ","))
+	fundResult, err := FetchFunds(ctx, strings.Split(fundCodes, ","))
 	if err != nil {
 		log.Fatalf("failed to fetch funds, err:%s", err)
 		return
 	}
 	// judge
 	const minRiseNum, maxFallNum = 1, -0.8
-	content, shouldSend := GenerateEmailHTML(fundResult, minRiseNum, maxFallNum)
-	log.Printf("shouldSend:%v", shouldSend)
-	if !shouldSend {
-		return
-	}
-	// notify
-	err = SendEmail(content, smtpHost, emailName, emailPassword, emailTo)
-	if err != nil {
-		log.Fatalf("failed to send email, err:%s", err)
-		return
+	errGroup, ctx := errgroup.WithContext(ctx)
+	// notify via email
+	errGroup.Go(func() error {
+		content, shouldSend := GenerateEmailHTML(ctx, fundResult, minRiseNum, maxFallNum)
+		log.Printf("shouldSend email:%v", shouldSend)
+		if smtpHost == "" || emailName == "" || emailPassword == "" || emailTo == "" {
+			shouldSend = false
+			log.Printf("shouldSend email:%v", shouldSend)
+		}
+		if !shouldSend {
+			return nil
+		}
+		err = SendEmail(ctx, content, smtpHost, emailName, emailPassword, emailTo)
+		if err != nil {
+			log.Printf("[E]failed to send email, err:%s", err)
+			return err
+		}
+		return nil
+	})
+	// notify via serverChan
+	errGroup.Go(func() error {
+		title, body, shouldSend := GenerateServerChanMessage(ctx, fundResult, minRiseNum, maxFallNum)
+		log.Printf("shouldSend serverChan :%v", shouldSend)
+		if serverChanKey == "" {
+			shouldSend = false
+			log.Printf("shouldSend serverChan:%v", shouldSend)
+		}
+		if !shouldSend {
+			return nil
+		}
+		// notify via email
+		err = SendServerChan(ctx, title, body, serverChanKey)
+		if err != nil {
+			log.Printf("[E]failed to send serverChan, err:%s", err)
+			return err
+		}
+		return nil
+	})
+	if err := errGroup.Wait(); err != nil {
+		log.Fatal(err)
 	}
 }
